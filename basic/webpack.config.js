f70d37bebbb52214f314af9cc8f533c905f6f079
chendanhua <chendanhua@jianke.com>
2020-03-11 00:19:25 +0800

    first commit

---
+++ basic/webpack.config.js
@@ -0,0 +1,56 @@
+const path = require('path');
+const UglifyPlugin = require('uglifyjs-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        include: [
+          path.resolve(__dirname, 'src')
+        ],
+        use: 'babel-loader',
+      },
+      {
+        test: /\.less$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            'css-loader', 
+            'less-loader',
+          ],
+        })
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader'
+          },
+        ],
+      },
+    ]
+  },
+  resolve: {
+    modules: [
+      "node_modules",
+      path.resolve(__dirname, 'src')
+    ],
+    extensions: [".wasm", ".mjs", ".js", ".json", ".jsx"],
+  },
+  plugins: [
+    new UglifyPlugin(), // 压缩js文件
+    new HtmlWebpackPlugin({
+      title: 'dandan',
+      template: 'src/index.html',
+    }), // hash
+    new ExtractTextPlugin('[name].css'),
+  ]
+}
